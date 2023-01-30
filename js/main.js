89b8e52a94b78b633d272343dd7a0cddb5b5fa4d
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-01-30 19:21:39 +0200

    Contacts-done
    Contacts-done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-$(function(){
-
+$(function () {
+    $('.slider__box').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>'
+    })
 });
