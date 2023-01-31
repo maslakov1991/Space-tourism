c4cfb73801cba92784ad9975380f8fc713d6ec66
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-01-31 16:22:20 +0200

    header-adaptive-done
    header-adaptive-done

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function () {
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>'
     })
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('active');
+    });
 });
