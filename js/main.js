a4b2bbcf2d26de037f762148af99b544a3f66941
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-02-01 12:55:05 +0200

    Adaptive-done
    Adaptive-done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(function () {
     $('.slider__box').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>'
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 481,
+                settings:{
+                    arrows: false,
+                    autoplay: 3000
+                }
+        }
+    ]
+        
     })
 
     $('.menu__btn').on('click', function () {
